6a7069ab7df8a0e414bb42f7dc8e2fff38baf0a1
Drew Anello <drew.anello@gmail.com>
2024-12-09 15:14:11 -0500

    wip

--- zip_lambda.sh
+++ zip_lambda.sh
@@ -6,8 +6,11 @@ ZIP_FILE="costanza_lambda.zip"
 # Remove any existing zip file
 rm -f $ZIP_FILE
 
+(cd lib; zip ../$ZIP_FILE -r .)
+
+cd .. 
 # Zip the FastAPI project files
-zip -r $ZIP_FILE main.py models.py database.py requirements.txt Dockerfile
+zip -u $ZIP_FILE main.py models.py database.py requirements.txt Dockerfile
 
 # Print a message indicating the zip file has been created
 echo "Created $ZIP_FILE for deployment."
