82b0810b045c8289d3127f2c28b953db88bd3b76
Drew Anello <drew.anello@gmail.com>
2024-12-09 14:34:50 -0500

    wip

--- zip_lambda.sh
+++ zip_lambda.sh
@@ -6,8 +6,8 @@ ZIP_FILE="costanza_lambda.zip"
 # Remove any existing zip file
 rm -f $ZIP_FILE
 
-# # Zip the FastAPI project files
-# zip -r $ZIP_FILE main.py models.py database.py requirements.txt
+# Zip the FastAPI project files
+zip -r $ZIP_FILE main.py models.py database.py requirements.txt
 
-# # Print a message indicating the zip file has been created
-# echo "Created $ZIP_FILE for deployment."
+# Print a message indicating the zip file has been created
+echo "Created $ZIP_FILE for deployment."
